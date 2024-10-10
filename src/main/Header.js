5973069aaa885fed6e22edd28d4f358aaab626f8
tetrisTESTER22 <niepielievdaniil@gmail.com>
2024-10-10 19:40:50 +0300

    add form with information about loggined user

--- src/main/Header.js
+++ src/main/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './maincss/Header.css'; 
 
-const Header = () => {
+const Header = ({ username, amount, userId, level, countOfReferrals, currentLevel, nextLevel }) => {
   return (
-    <header>
-      <h1>Заголовок страницы</h1>
+    <header className="header-container">
+      <div className="header-border">
+        <pre>
+          {`
+icon ${username.padEnd(20)} Balance: $${amount}
+
+ User ID: ${userId}
+ Level: ${level}
+ Referals: ${countOfReferrals}`}
+        </pre>
+      </div>
+      <div>
+        <div className="progress-bar-container">
+          <div className="progress-bar"></div>
+        </div>
+        <div className="level-container">
+          <span>{currentLevel}</span>
+          <span>{nextLevel}</span>
+        </div>
+      </div>
     </header>
   );
 }
--- src/main/MainPage.js
+++ src/main/MainPage.js
@@ -5,8 +5,17 @@ import Footer from './Footer';
 
 function MainPage() {
   return (
-    <div>
-      <Header />
+    <div style={{ display: 'block', flexDirection: 'column', alignItems: 'center' }}>
+      <Header
+      username="username123"
+      amount={150.75}
+      userId={12345}
+      level={5}
+      countOfReferrals={10}
+      currentLevel="lvl3"
+      nextLevel="lvl4"
+    />
+    
       <Body />
       <Footer />
     </div>
