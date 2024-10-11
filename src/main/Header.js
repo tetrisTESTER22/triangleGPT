e665d710aa0ef5a25334065fde0869c9407616b6
tetrisTESTER22 <niepielievdaniil@gmail.com>
2024-10-11 11:20:31 +0300

    done header main page

--- src/main/Header.js
+++ src/main/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './maincss/Header.css'; 
-
+import iconPath from './mainPageIcons/userIcon.png';
 const Header = ({ username, amount, userId, level, countOfReferrals, currentLevel, nextLevel }) => {
   return (
     <header className="header-container">
       <div className="header-border">
-        <pre>
-          {`
-icon ${username.padEnd(20)} Balance: $${amount}
-
- User ID: ${userId}
- Level: ${level}
- Referals: ${countOfReferrals}`}
-        </pre>
+        <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap'}}>
+          <img src={iconPath} alt="icon" className='userIconImage' />
+          <span className='userNameSpan'>{`${username.padEnd(20)}`}</span>
+          <span className='balanceSpan'>Balance: ${amount}</span>
+        </div>
+        <div>
+          <p>User ID: {userId}</p>
+          <p>Level: {level}</p>
+          <p>Referrals: {countOfReferrals}</p>
+        </div>
       </div>
       <div>
         <div className="progress-bar-container">
@@ -25,6 +27,6 @@ icon ${username.padEnd(20)} Balance: $${amount}
       </div>
     </header>
   );
-}
+};
 
 export default Header;
